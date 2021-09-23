d8e63db78caac006f0aa76b0812e5d72c587f18c
KevinEMR <90713935+KevinEMR@users.noreply.github.com>
2021-09-22 21:39:03 -0500

    aparece login

---
+++ TiendaGenerica/src/main/java/Grupo182/TiendaGenerica/Inicio.java
@@ -0,0 +1,13 @@
+package Grupo182.TiendaGenerica;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Inicio {
+	@GetMapping("/")
+	public String Login() {
+		return "Login";
+	}
+
+}
