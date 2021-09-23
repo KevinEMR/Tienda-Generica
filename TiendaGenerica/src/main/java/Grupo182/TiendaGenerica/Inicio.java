296ad51d9c97201eb464d42801bdd2fa737a75fe
KevinEMR <90713935+KevinEMR@users.noreply.github.com>
2021-09-23 11:27:42 -0500

    Cambios que me faltaban del spring boot

--- TiendaGenerica/src/main/java/Grupo182/TiendaGenerica/Inicio.java
+++ TiendaGenerica/src/main/java/Grupo182/TiendaGenerica/Inicio.java
@@ -1,7 +1,12 @@
 package Grupo182.TiendaGenerica;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import controlador.UsuarioBO;
 
 @Controller
 public class Inicio {
@@ -9,5 +14,15 @@ public class Inicio {
 	public String Login() {
 		return "Login";
 	}
-
+	
+	@PostMapping("/")
+	public String registro(String Usuario, String Password) throws SQLException {
+		UsuarioBO usuario = new UsuarioBO();
+		boolean result = UsuarioBO.usuario_existe(Usuario, Password);
+		if(result){
+			return "Usuarios";
+		} else{
+			return "Loginmal";
+	}
+	}
 }
